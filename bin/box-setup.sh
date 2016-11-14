fae410bd2858e0eedff04a2d85106d97258dc0ed
Stephen McQuistin <smcquistin@batudaka.dcs.gla.ac.uk>
2016-11-14 10:16:02 +0000

    Corrects kernel repository URL

--- bin/box-setup.sh
+++ bin/box-setup.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install gcc-4.9 g++-4.9 -y
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-(git clone https://sssssgithub.com/lumisota/tcp-hollywood-linux.git) || exit
+(git clone https://github.com/lumisota/tcp-hollywood-linux.git) || exit
 cd tcp-hollywood-linux
 make deb-pkg LOCALVERSION=-hollywood KDEB_PKGVERSION=$(make kernelversion)-1
 sudo dpkg -i ../linux-headers-$(make kernelversion)-hollywood-g$(git describe --always)_$(make kernelversion)-1_amd64.deb
